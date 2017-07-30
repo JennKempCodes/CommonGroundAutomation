fff074851776eaac8c969f76908bc5016ac62d98
joshua kemp <joshuakemp85@gmail.com>
2017-07-30 16:36:34 -0400

    Cleaning up the variables in script

--- commonground_smoke.rb
+++ commonground_smoke.rb
@@ -1,9 +1,33 @@
 require 'selenium-webdriver'
-require 'rspec/expectations'
-include RSpec::Matchers
+# require 'rspec/expectations'
+# include RSpec::Matchers
+
+@commonground_homeurl = "http://www.commongroundshreveport.com/"
 
 @driver = Selenium::WebDriver.for :firefox
-@driver.get "http://www.commongroundshreveport.com/"
+@driver.get @commonground_homeurl
 # expect(@driver.find_element(id: 'login').displayed?).to eql false
 
-expect(@driver).to have_current_url(@people, "http://www.commongroundshreveport.com/": true)
+puts "This is the current URL: " + @driver.current_url
+
+if @driver.current_url == @commonground_homeurl
+  puts "current url is correct!"
+  send_success_email
+else
+  puts "Sorry Captain! this URL is wrong!"
+  send_failure_email
+  abort
+end
+
+# @commonground_homeurl = .to have_current_url(@people, "http://www.commongroundshreveport.com/": true)
+
+
+# expect(@driver).to have_current_url(@people, "http@commonground_homeurl
+
+
+def send_failure_email
+end
+
+def send_success_email
+
+end
