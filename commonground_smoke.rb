3d17d788d5db84bebda3ebe5cf339093f1f7d598
“Jennifer <jenn.kemp.codes@gmail.com>
2017-07-30 22:24:46 -0400

    Cleaning code and adding success or failure email

--- commonground_smoke.rb
+++ commonground_smoke.rb
@@ -1,19 +1,45 @@
 require 'selenium-webdriver'
+require 'gmail'
+require_relative 'Password'
 # require 'rspec/expectations'
 # include RSpec::Matchers
 
 @commonground_initial_url = 'http://commongroundshreveport.com/'
-@commonground_homeurl = 'http://www.commongroundshreveport.com/'
+@commonground_homeurl = 'http://www.commongroundshrevepor.com/'
+@username = 'EMAIL'
+@password = 'PASSWORD'
 
 @driver = Selenium::WebDriver.for :firefox
 @driver.navigate.to @commonground_initial_url
+
 # expect(@driver.find_element(id: 'login').displayed?).to eql false
 
 puts 'This is the current URL: ' + @driver.current_url
 
+def send_email(status)
+  @gmail = Gmail.connect(@username, @password)
+
+  email = @gmail.compose do
+    to 'Jenn.Kemp.Codes@gmail.com'
+
+    if status == 'success'
+     subject 'Your website is up and running!'
+      body 'We have succesfully pinged your website and it is up and working'
+      puts 'Success email has been sent'
+    else
+      subject 'Your website is not up working'
+      body 'We have pinged your website and it is not functioning properly'
+      puts 'Failure email has been sent'
+      end
+  end
+  email.deliver!
+end
+
 if @driver.current_url == @commonground_homeurl
   puts 'current url is correct!'
+  send_email('success')
+
 else
   puts 'Sorry Captain! this URL is wrong!'
-  abort
+  send_email('failure')
 end
