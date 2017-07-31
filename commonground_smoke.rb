ea1f75907521b6c7b9c5aee06b4493984727d31b
“Jennifer <jenn.kemp.codes@gmail.com>
2017-07-30 20:32:12 -0400

    Cleaning up code for best practices

--- commonground_smoke.rb
+++ commonground_smoke.rb
@@ -2,32 +2,18 @@ require 'selenium-webdriver'
 # require 'rspec/expectations'
 # include RSpec::Matchers
 
-@commonground_homeurl = "http://www.commongroundshreveport.com/"
+@commonground_initial_url = 'http://commongroundshreveport.com/'
+@commonground_homeurl = 'http://www.commongroundshreveport.com/'
 
 @driver = Selenium::WebDriver.for :firefox
-@driver.get @commonground_homeurl
+@driver.navigate.to @commonground_initial_url
 # expect(@driver.find_element(id: 'login').displayed?).to eql false
 
-puts "This is the current URL: " + @driver.current_url
+puts 'This is the current URL: ' + @driver.current_url
 
 if @driver.current_url == @commonground_homeurl
-  puts "current url is correct!"
-  # send_success_email
+  puts 'current url is correct!'
 else
-  puts "Sorry Captain! this URL is wrong!"
-  # send_failure_email
+  puts 'Sorry Captain! this URL is wrong!'
   abort
 end
-
-# @commonground_homeurl = .to have_current_url(@people, "http://www.commongroundshreveport.com/": true)
-
-
-# expect(@driver).to have_current_url(@people, "http@commonground_homeurl
-
-
-# def send_failure_email
-# end
-
-# def send_success_email
-
-# end
